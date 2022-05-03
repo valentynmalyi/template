df3dbc015b88d1f7eaf609a8368766fadeed9c20
Valentyn Malyi <valentyn.malyi@gcorelabs.com>
2022-05-03 16:55:35 +0300

    task add vectors

--- test/test_multiply.py
+++ test/test_multiply.py
@@ -5,3 +5,12 @@ def test_multiply():
     v1 = Vector(1, 1)
     v2 = Vector(1, 2)
     assert multiply(v1, v2) == 3
+
+
+# class TestVector:
+#     def test__add__(self):
+#         v1 = Vector(1, 1)
+#         v2 = Vector(1, 2)
+#         v3 = v1 + v1
+#         assert v3.x == 2
+#         assert v3.y == 3
