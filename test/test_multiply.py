366ece20b8725a8069026befb1c04dfe139a9855
Valentyn Malyi <valentyn.malyi@gcorelabs.com>
2022-05-03 17:18:48 +0300

    conflict

--- test/test_multiply.py
+++ test/test_multiply.py
@@ -6,11 +6,4 @@ def test_multiply():
     v2 = Vector(1, 2)
     assert multiply(v1, v2) == 3
 
-
-# class TestVector:
-#     def test__add__(self):
-#         v1 = Vector(1, 1)
-#         v2 = Vector(1, 2)
-#         v3 = v1 + v1
-#         assert v3.x == 2
-#         assert v3.y == 3
+# asdasd
