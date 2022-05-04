3bcea23faa4688380be130509f447df9183230b9
Ian <cupofcoffeewithlime1@gmail.com>
2022-05-04 21:18:52 +0300

    add test for function "create_vector"

--- test/test_multiply.py
+++ test/test_multiply.py
@@ -1,4 +1,5 @@
 from vector import Vector
+from point import Point, LineSegment
 
 
 class TestVector:
@@ -14,3 +15,11 @@ class TestVector:
         v2 = Vector(1, 2)
         res = v1 * v2
         assert res == 3
+
+
+class TestLineSegment:
+    def test_create_vector(self):
+        a = Point(1, 1)
+        b = Point(2, 1)
+        res = LineSegment(a, b).create_vector()
+        assert res.x == 1, res.y == 0
