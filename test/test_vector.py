f8b3c03684b99bcf1e421876e6a4f4901032b999
Ian <cupofcoffeewithlime1@gmail.com>
2022-05-05 14:11:47 +0300

    add test __eq__ for class Vector

--- test/test_vector.py
+++ test/test_vector.py
@@ -14,3 +14,8 @@ class TestVector:
         v2 = Vector(1, 2)
         res = v1 * v2
         assert res == 3
+
+    def test__eq__(self):
+        v1 = Vector(1, 1)
+        v2 = Vector(1, 1)
+        assert v1 == v2
