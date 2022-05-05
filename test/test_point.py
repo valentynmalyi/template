39a1feeeabf38707bb03e9e438c2107b3863e160
Ian <cupofcoffeewithlime1@gmail.com>
2022-05-05 14:11:19 +0300

    test modules split

---
+++ test/test_point.py
@@ -0,0 +1,10 @@
+from point import Point, LineSegment
+from vector import Vector
+
+
+class TestLineSegment:
+    def test_vector_property(self):
+        a = Point(1, 1)
+        b = Point(2, 1)
+        vector = LineSegment(a, b).vector
+        assert vector == Vector(1, 0)
--- test/test_vector.py
+++ test/test_vector.py
@@ -1,5 +1,4 @@
 from vector import Vector
-from point import Point, LineSegment
 
 
 class TestVector:
@@ -15,11 +14,3 @@ class TestVector:
         v2 = Vector(1, 2)
         res = v1 * v2
         assert res == 3
-
-
-class TestLineSegment:
-    def test_vector_property(self):
-        a = Point(1, 1)
-        b = Point(2, 1)
-        vector = LineSegment(a, b).vector
-        assert vector == Vector(1, 0)
