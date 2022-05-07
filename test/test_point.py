1afceb6e6a7ce1a73b28a4a49a97cd7652180af7
Ian <cupofcoffeewithlime1@gmail.com>
2022-05-07 16:50:26 +0300

    add tests for length and square length methods, test vector method refactor

--- test/test_point.py
+++ test/test_point.py
@@ -1,10 +1,15 @@
+from math import isclose
+
 from point import LineSegment, Point
 from vector import Vector
 
 
 class TestLineSegment:
     def test_vector_property(self):
-        a = Point(1, 1)
-        b = Point(2, 1)
-        vector = LineSegment(a, b).vector
-        assert vector == Vector(1, 0)
+        assert LineSegment(Point(1, 1), Point(2, 1)).vector == Vector(1, 0)
+
+    def test_square_length_property(self):
+        assert LineSegment(Point(1, 1), Point(2, 2)).vector.square_length == 2
+
+    def test_length_property(self):
+        assert isclose(LineSegment(Point(2, 2), Point(1, 1)).vector.length, 2 ** 0.5)
