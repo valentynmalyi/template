37792005e4b789462e57889ce53edd8fa1f8519a
Ian <cupofcoffeewithlime1@gmail.com>
2022-05-09 12:55:23 +0300

    rename RightTriangle init function

--- triangle.py
+++ triangle.py
@@ -16,9 +16,9 @@ class Triangle:
 class RightTriangle(Triangle):
     def __init__(self, a: Point, b: Point, c: Point):
         super().__init__(a=a, b=b, c=c)
-        self._init_triangle(a, b, c)
+        self._validate_is_right(a, b, c)
 
-    def _init_triangle(self, a: Point, b: Point, c: Point) -> None:
+    def _validate_is_right(self, a: Point, b: Point, c: Point) -> None:
         side = sorted([LineSegment(a, b).length, LineSegment(b, c).length, LineSegment(a, c).length])
         if not isclose(side[2] ** 2, side[1] ** 2 + side[0] ** 2):
             raise Exception(f"triangle {self} isn't a right triangle")
