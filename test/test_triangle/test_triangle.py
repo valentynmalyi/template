f0ce587b56f2f7fa0805701bc793e93ace60d0f1
Ian <cupofcoffeewithlime1@gmail.com>
2022-05-12 19:52:47 +0300

    added tests for Triangle exceptions IsLine, IsPoint

--- test/test_triangle/test_triangle.py
+++ test/test_triangle/test_triangle.py
@@ -1,7 +1,19 @@
 import pytest
+from itertools import permutations
 
 from point import Point
-from triangle.triangle import RightTriangle, Triangle
+from triangle.triangle import exceptions, RightTriangle, Triangle
+
+
+class TestTriangle:
+    def test_validate_is_line(self):
+        with pytest.raises(exceptions.IsLine):
+            for a, b, c in permutations([Point(0, 0), Point(0, 0), Point(0, 1)], 3):
+                Triangle(a, b, c)
+
+    def test_validate_is_point(self):
+        with pytest.raises(exceptions.IsPoint):
+            RightTriangle(Point(0, 0), Point(0, 0), Point(0, 0))
 
 
 class TestRightTriangle:
