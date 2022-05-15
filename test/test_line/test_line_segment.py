70ec580b60cbe88f430362227c176c90aa6f8ce2
Ian <cupofcoffeewithlime1@gmail.com>
2022-05-15 18:58:42 +0300

    add test for equal_points validation

--- test/test_line/test_line_segment.py
+++ test/test_line/test_line_segment.py
@@ -1,7 +1,9 @@
 from math import isclose
 
+import pytest
+
 from line import Vector
-from line.line_segment import LineSegment
+from line.line_segment import LineSegment, exceptions
 from point import Point
 
 
@@ -14,3 +16,8 @@ class TestLineSegment:
 
     def test_length(self):
         assert isclose(LineSegment(Point(2, 2), Point(1, 1)).length, 2 ** 0.5)
+
+    def test_validate_equal_points(self):
+        LineSegment(Point(0, 0), Point(1, 1))
+        with pytest.raises(exceptions.EqualPoints):
+            LineSegment(Point(0, 0), Point(0, 0))
