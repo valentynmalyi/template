e97ed7796dfe5285aa4e366ab5750b837f3e090d
Ian <cupofcoffeewithlime1@gmail.com>
2022-05-19 18:13:54 +0300

    refactored triangle_tests module

--- test/test_triangle/test_triangle.py
+++ test/test_triangle/test_triangle.py
@@ -3,18 +3,20 @@ from unittest.mock import call, patch
 import pytest
 
 from point import Point
-from triangle.triangle import exceptions, RightTriangle, Triangle
+from triangle import exceptions
+from triangle.triangle import RightTriangle, Triangle
+from triangle.validations import TriangleValidation
 
 
 class TestTriangle:
     def test_validate_is_point(self):
-        Triangle._validate_is_point(Point(1, 1), Point(0, 0), Point(1, 0))
+        Triangle(Point(1, 1), Point(0, 0), Point(1, 0))
         with pytest.raises(exceptions.IsPoint):
-            Triangle._validate_is_point(Point(0, 0), Point(0, 0), Point(0, 0))
+            Triangle(Point(0, 0), Point(0, 0), Point(0, 0))
 
-    @patch.object(Triangle, "_validate_line_segment")
+    @patch.object(TriangleValidation, "_validate_line_segment")
     def test_validate_is_line(self, mock_line_segment):
-        Triangle._validate_is_line(Point(1, 1), Point(0, 0), Point(1, 0))
+        Triangle(Point(1, 1), Point(0, 0), Point(1, 0))
         assert len(mock_line_segment.mock_calls) == 6
         calls = [
             call(Point(1, 1), Point(0, 0), Point(1, 0)),
@@ -27,21 +29,21 @@ class TestTriangle:
         mock_line_segment.assert_has_calls(calls, any_order=True)
 
     def test_validate_line_segment(self):
-        Triangle._validate_line_segment(Point(1, 1), Point(0, 0), Point(1, 0))
+        Triangle(Point(1, 1), Point(0, 0), Point(1, 0))
         with pytest.raises(exceptions.IsLine):
-            Triangle._validate_line_segment(Point(0, 0), Point(0, 0), Point(1, 1))
+            Triangle(Point(0, 0), Point(0, 0), Point(1, 1))
 
     def test_validate_triangle_inequality(self):
-        Triangle._validate_triangle_inequality(Point(1, 1), Point(0, 0), Point(1, 0))
+        Triangle(Point(1, 1), Point(0, 0), Point(1, 0))
         with pytest.raises(exceptions.IsLine):
-            Triangle._validate_triangle_inequality(Point(1, 2), Point(2, 4), Point(4, 8))
-            Triangle._validate_triangle_inequality(Point(1, 3), Point(2, 6), Point(3, 9))
-            Triangle._validate_triangle_inequality(Point(0, 0), Point(0, 1), Point(0, 2))
+            Triangle(Point(1, 2), Point(2, 4), Point(4, 8))
+            Triangle(Point(1, 3), Point(2, 6), Point(3, 9))
+            Triangle(Point(0, 0), Point(0, 1), Point(0, 2))
 
 
 class TestRightTriangle:
     def test_validate_is_right(self):
-        RightTriangle._validate_is_right(Point(0, 0), Point(1, 0), Point(0, 2))
         # assert isinstance(RightTriangle(Point(0, 0), Point(1, 0), Point(0, 2)), Triangle)
+        RightTriangle(Point(0, 0), Point(1, 0), Point(0, 2))
         with pytest.raises(exceptions.IsNotRight):
-            RightTriangle._validate_is_right(Point(3, 3), Point(1, 0), Point(0, 2))
+            RightTriangle(Point(3, 3), Point(1, 0), Point(0, 2))
